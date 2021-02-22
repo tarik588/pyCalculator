f1440c933c6dc8ea5cc44e36784ccc3079d338d6
tarik588 <60273808+tarik588@users.noreply.github.com>
2021-02-22 09:04:57 -0500

    Update test_Calculator.py

--- tests/test_Calculator.py
+++ tests/test_Calculator.py
@@ -22,11 +22,33 @@ class CalculatorTestCase(unittest.TestCase):
         tester = csvRead(..tests/Unit Test Subtration.csv)
         for row in test:
             result = int(row['results']
-            self.assertEqual(self.calculator.add(row['value 1', row['value 2], result)
+            self.assertEqual(self.calculator.subtract(row['value 1', row['value 2], result)
              
         def multitest(self):
-        tester = csvRead(..tests/Unit Test Multiplication.csv)
-
+            tester = csvRead(..tests/Unit Test Multiplication.csv)
+            for row in test:
+                 result = int(row['results']
+                 self.assertEqual(self.calculator.muliply(row['value 1', row['value 2], result)
+                                                                         
+        def divitest(self):
+            tester = csvRead(..tests/Unit Test Division.csv)
+            for row in test:
+                 result = int(row['results']
+                 self.assertEqual(self.calculator.divide(row['value 1', row['value 2], result)
+        
+         def sqrttest(self):
+            tester = csvRead(..tests/Unit Test Sqrt.csv)
+            for row in test:
+                 result = int(row['results']
+                 self.assertEqual(self.calculator.sqrt(row['value 1'], result)
+         
+         def squaretest(self):
+         tester = csvRead(..tests/Unit Test Square.csv)
+         for row in test:
+              result = int(row['results']
+              self.assertEqual(self.calculator.square(row['value 1'], result)
+        
+        
                                                                    
 
 print()
