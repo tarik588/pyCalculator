62218b5e7e044b8aea741c2c4b626e2b14ab006a
tarik588 <60273808+tarik588@users.noreply.github.com>
2021-02-22 08:59:33 -0500

    Update test_Calculator.py

--- tests/test_Calculator.py
+++ tests/test_Calculator.py
@@ -1,5 +1,4 @@
 import unittest
-
 from src.calculator.calculator import Calculator
 
 
@@ -12,6 +11,23 @@ class CalculatorTestCase(unittest.TestCase):
 
     def test_result_is_zero_calculator(self):
         self.assertEqual(self.calculator.result, 0)
+   
+    def addtest(self):
+        tester = csvRead(..tests/Unit Test Additon.csv)
+        for row in test:
+            result = int(row['results']
+            self.assertEqual(self.calculator.add(row['value 1', row['value 2], result)
+                                    
+      def subtracttest(self):
+        tester = csvRead(..tests/Unit Test Subtration.csv)
+        for row in test:
+            result = int(row['results']
+            self.assertEqual(self.calculator.add(row['value 1', row['value 2], result)
+             
+        def multitest(self):
+        tester = csvRead(..tests/Unit Test Multiplication.csv)
+
+                                                                   
 
 print()
 
